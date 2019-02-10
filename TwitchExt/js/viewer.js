e0296579055cd5c423c81f451fbb32b520766e61
Arkanias566 <arkan565@users.noreply.github.com>
2019-02-10 19:02:53 +0100

    design changes for the extension
    Added a light and dark mode and a menu for the extension

--- TwitchExt/js/viewer.js
+++ TwitchExt/js/viewer.js
@@ -1,9 +1,12 @@
 function getData() {
     $.get('https://api.coindesk.com/v1/bpi/currentprice.json', function(data){
-        $('body').html(data);
+        $('#content').html(data);
     })
 }
 $(window).on('load', function () {
     getData();
     window.Twitch.ext.rig.log('...')
+    $('#lightTurn').click(function(){
+        $('body').toggleClass('dark')
+    })
 })
